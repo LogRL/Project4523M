74f08d537bed86f837e740155fbc21eda50dcf78
Nanatangoku <ku123912399asb@gmail.com>
2024-05-19 00:39:08 +0800

    btn search change to logout

--- manager/manager_insert_Item.php
+++ manager/manager_insert_Item.php
@@ -18,46 +18,50 @@
 </head>
 
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
         <div class="container-fluid ">
-          <a class="navbar-brand" href="#">
-            <i class="bi bi-x-diamond-fill"></i>
-          </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse " id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="manager_home.html">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="manager_update_record.php">Update Order records</i></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="manager_generate_report.php">Generate Report</i></a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Item
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a class="dropdown-item" href="manager_insert_Item.php">Insert Item</a></li>
-                  <li><a class="dropdown-item" href="manager_edit_Item.php">Edit Item</a></li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li><a class="dropdown-item" href="manager_delete_Item.php">Delete Item</a></li>
+            <a class="navbar-brand" href="#">
+                <i class="bi bi-x-diamond-fill"></i>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse " id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="manager_home.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="manager_update_record.php">Update Order records</i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="manager_generate_report.php">Generate Report</i></a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Item
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="manager_insert_Item.php">Insert Item</a></li>
+                            <li><a class="dropdown-item" href="manager_edit_Item.php">Edit Item</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="manager_delete_Item.php">Delete Item</a></li>
+                        </ul>
+                    </li>
+
                 </ul>
-              </li>
-            
-            </ul>
-            <form class="d-flex">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+                <form class="d-flex">
+                    <button class="btn btn-outline-success" type="button">Logout</button>
+                </form>
             </div>
         </div>
-      </nav>
-      <div class="container">
+    </nav>
+    <div class="container">
         <div class="row">
             <div class=" forms1 col">
                 <form>
