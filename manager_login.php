d6c3e6d2ee92f315453e463cf5e1aa2be5f3ea6c
LogRL <jack1242003@gmail.com>
2024-06-30 01:56:59 +0800

    login page fix db connection

--- manager_login.php
+++ manager_login.php
@@ -4,7 +4,7 @@
     $manager_name = $_POST['manager_name'];
     $manager_pw = $_POST['manager_pw'];
 
-    $sql = "SELECT * FROM salesmanager WHERE managerName = '$manager_name' AND password = '$manager_pw'";
+    $sql = "SELECT * FROM sales_manager WHERE sm_name = '$manager_name' AND pwd = '$manager_pw'";
 
     if($result = mysqli_query($conn, $sql))
     {
@@ -13,21 +13,16 @@
             echo "success";
             session_start();
             $rs = mysqli_fetch_array($result);
-           
-            
         }
         else
         {
             echo "wrong account name or password";
         }
-
     }
     else
     {
         echo "database connet error";
     }
-    
 
     mysqli_close($conn);
-    
 ?>
